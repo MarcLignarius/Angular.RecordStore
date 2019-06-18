d7fa710216cfab97f7c3a46816643202329131b4
Dustin Herboldshimer and Marc Davies <dustin@>
2019-06-18 11:00:05 -0700

    initial commit

---
+++ src/app/marketplace/marketplace.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
+
+
+@Component({
+  selector: 'app-marketplace',
+  templateUrl: './marketplace.component.html',
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
+})
+
+export class MarketplaceComponent implements OnInit {
+  constructor(private router: Router, private albumService: AlbumService){}
+  albums: Album[];
+  ngOnInit(){
+    this.albums = this.albumService.getAlbums();
+  }
+  goToDetailPage(clickedAlbum: Album) {
+    this.router.navigate(['albums', clickedAlbum.id]);
+  };
+}
